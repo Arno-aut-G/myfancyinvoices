3491002f0f2f3b6c9c5a5a04657af20d1f9a22be
Arno Goebel <arno.goebel@seitenbau.de>
2022-07-09 13:25:40 +0200

    03 Spring WebMVC

--- src/main/java/com/dachsbau/myfancypdfinvoices/context/ApplicationConfiguration.java
+++ src/main/java/com/dachsbau/myfancypdfinvoices/context/ApplicationConfiguration.java
@@ -1,15 +1,12 @@
 package com.dachsbau.myfancypdfinvoices.context;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.Scope;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.dachsbau.myfancypdfinvoices.ApplicationLauncher;
-import com.dachsbau.myfancypdfinvoices.service.InvoiceService;
-import com.dachsbau.myfancypdfinvoices.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
@@ -17,7 +14,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @PropertySource("classpath:/application.properties")
 @PropertySource(value = "classpath:/application-${spring.profiles.active}.properties"
         , ignoreResourceNotFound = true)
-public class MyFancyInvoicesApplicationConfiguration {
+@EnableWebMvc
+public class ApplicationConfiguration {
 
     @Bean
     public ObjectMapper objectMapper() {
